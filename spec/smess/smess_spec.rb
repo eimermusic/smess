4590d11075b24fc6323bb5442a79709eeec5c984
Martin Westin <martin@eimermusic.com>
2015-08-31 16:27:32 +0200

    starting to move from constants to config object

--- spec/smess/smess_spec.rb
+++ spec/smess/smess_spec.rb
@@ -1,20 +1,43 @@
 require 'spec_helper'
 
-describe "Smess.output", iso_id: "7.1" do
+describe Smess do
+  describe "#output" do
+    let(:sms) {
+      Smess.new(
+        to: '46701234567',
+        message: 'Test SMS',
+        originator: 'TestSuite',
+        output: "test"
+      )
+    }
+    it "returns a populated Sms when mobule macro is invoked" do
+      sms.class.should == Smess::Sms
+      sms.to.should == '46701234567'
+      sms.output.should == 'test'
+    end
+  end
 
-  let(:sms) {
-    Smess.new(
-      to: '46701234567',
-      message: 'Test SMS',
-      originator: 'TestSuite',
-      output: "test"
-    )
-  }
+  describe "Config behavior" do
+    it "reads config defaults" do
+      expect(Smess.config.debug).to be_false
+    end
 
-  it 'returns a populated Sms when mobule macro is invoked' do
-    sms.class.should == Smess::Sms
-    sms.to.should == '46701234567'
-    sms.output.should == 'test'
+    it "reads changed config values" do
+      Smess.configure do |config|
+        config.debug = true
+      end
+      expect(Smess.config.debug).to be_true
+    end
+
+    it "can reset config back to defaults" do
+      Smess.configure do |config|
+        config.debug = true
+      end
+      expect(Smess.config.debug).to be_true
+      Smess.reset_config
+      expect(Smess.config.debug).to be_false
+    end
+
   end
 
 end
